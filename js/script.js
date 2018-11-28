0b56434a1c0ed5ab9c343669237dc01d3033fcd3
Ronny P.A <ronny.polloqueri@gmail.com>
2018-11-28 17:01:02 -0500

    Meets Expectations

--- js/script.js
+++ js/script.js
@@ -16,7 +16,34 @@ FSJS project 1 - A Random Quote Generator
       quote object.
 ***/
 
-
+var quotes = [
+  {
+    "quote": "A champion is defined not by their wins but by how they can recover when they fall.",
+    "source": "Serena Williams",
+    "citation": "The National",
+    "year": "2012"
+  },
+  {
+    "quote": "Let us make our future now, and let us make our dreams tomorrow's reality.",
+    "source": "Malala Yousafzai"
+  },
+  {
+    "quote": "You must do the things you think you cannot do.",
+    "source": "Eleanor Roosevelt"
+  },
+  {
+    "quote": "Nothing is impossible. The word itself says \"I'm possible!\"",
+    "source": "Audrey Hepburn"
+  },
+  {
+    "quote": "Happiness is not by chance, but by choice.",
+    "source": "Jim Rohn"
+  },
+  {
+    "quote": "Don't wait. The time will never be just right.",
+    "source": "Napoleon Hill"
+  },
+]
 
 
 /***
@@ -26,7 +53,9 @@ FSJS project 1 - A Random Quote Generator
      `quotes` array.
 ***/
 
-
+function getRandomQuote(){
+  return quotes[Math.floor(Math.random()*quotes.length)];
+}
 
 
 /***
@@ -38,7 +67,22 @@ FSJS project 1 - A Random Quote Generator
      they are added to the HTML string.
    - set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
-
+function printQuote(){
+  var quote = getRandomQuote();
+  console.log(quote);
+  var quote_box = document.getElementById("quote-box");
+  var quote_html = quote_box.getElementsByClassName("quote")[0];
+  var source_html = quote_box.getElementsByClassName("source")[0];
+  var source = quote.source;
+  if(quote.citation != undefined) {
+    source += '<span class="citation">' + quote.citation +'</span>';
+  }
+  if(quote.year != undefined) {
+    source += '<span class="year">' + quote.year + '</span>';
+  }
+  quote_html.innerHTML = quote.quote;
+  source_html.innerHTML = source;
+}
 
 
 
@@ -50,6 +94,7 @@ FSJS project 1 - A Random Quote Generator
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
